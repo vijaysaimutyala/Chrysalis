023c62fe98c1807f696c7c807862e329c2c1b746
vijaysaimutyala <vijsuvig@gmail.com>
2017-06-29 20:59:22 +0530

    Update user profile

---
+++ app/src/main/java/com/studioemvs/chrysalis/UpdateProgressActivity.java
@@ -0,0 +1,13 @@
+package com.studioemvs.chrysalis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateProgressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_progress);
+    }
+}
